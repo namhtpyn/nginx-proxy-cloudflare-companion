7c550114af628882e84198d3bebb8023cb98568c
Nam Hoang <namht.pyn@gmail.com>
2024-05-15 07:33:35 +0000

    misc: add more log

--- src/index.ts
+++ src/index.ts
@@ -16,28 +16,33 @@ const job = new CronJob(
   //every minute
   "0 * * * * *",
   async () => {
-    const ipv4 = await publicIpv4();
-    if (cache.ipv4 === ipv4) return;
-    cache.ipv4 = ipv4;
+    try {
+      console.log("Running job");
+      const ipv4 = await publicIpv4();
+      if (cache.ipv4 === ipv4) return;
+      cache.ipv4 = ipv4;
 
-    const runningContainers = await docker.listContainers();
+      const runningContainers = await docker.listContainers();
 
-    const ids = runningContainers.map((container) => container.Id);
-    const hasNewContainer = ids.filter((id) => !cache.containerIds.includes(id)).length > 0;
-    if (!hasNewContainer) return;
-    cache.containerIds = ids;
+      const ids = runningContainers.map((container) => container.Id);
+      const hasNewContainer = ids.filter((id) => !cache.containerIds.includes(id)).length > 0;
+      if (!hasNewContainer) return;
+      cache.containerIds = ids;
 
-    for (const container of runningContainers) {
-      const containerInfo = await docker.getContainer(container.Id).inspect();
-      const env = await containerEnvSchema.safeParseAsync(
-        Object.fromEntries(containerInfo.Config.Env.map((e) => e.split("="))),
-      );
+      for (const container of runningContainers) {
+        const containerInfo = await docker.getContainer(container.Id).inspect();
+        const env = await containerEnvSchema.safeParseAsync(
+          Object.fromEntries(containerInfo.Config.Env.map((e) => e.split("="))),
+        );
 
-      if (!env.success) continue;
+        if (!env.success) continue;
 
-      await cloudflare.updateRecord({ domain: env.data.VIRTUAL_HOST, ipv4 });
+        await cloudflare.updateRecord({ domain: env.data.VIRTUAL_HOST, ipv4 });
 
-      console.log(`Updated ${env.data.VIRTUAL_HOST} to ${ipv4}`);
+        console.log(`Updated ${env.data.VIRTUAL_HOST} to ${ipv4}`);
+      }
+    } catch (e) {
+      console.log(e);
     }
   },
 );
