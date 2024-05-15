7050863deaaab3e8e702d79d0ff83eb00ecbd337
Nam Hoang <namht.pyn@gmail.com>
2024-05-15 12:41:44 +0000

    refactor: rework get running containers logic

--- src/docker.ts
+++ src/docker.ts
@@ -1,11 +1,25 @@
 import Dockerode from "dockerode";
+import { execSync } from "node:child_process";
 
 export const useDocker = () => {
   const docker = new Dockerode();
+  //get the id of the current container by executing the command `hostname` using shell
+  const currentContainerId = execSync("hostname").toString().trim();
 
   const getRunningContainers = async () => {
     const containers = await docker.listContainers();
-    return await Promise.all(containers.map((container) => docker.getContainer(container.Id).inspect()));
+    const currentContainer = containers.find((container) => container.Id.startsWith(currentContainerId));
+    if (!currentContainer) return [];
+
+    const currentNetworks = Object.keys(currentContainer.NetworkSettings.Networks);
+
+    const sameNetworkContainers = containers.filter(
+      (container) =>
+        container.Id !== currentContainer.Id &&
+        Object.keys(container.NetworkSettings.Networks).some((network) => currentNetworks.includes(network)),
+    );
+
+    return await Promise.all(sameNetworkContainers.map((container) => docker.getContainer(container.Id).inspect()));
   };
 
   return {
